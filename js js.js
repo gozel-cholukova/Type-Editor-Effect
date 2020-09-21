ff534f0c72491cae81b2e5e65cdac123916dd608
gozel-cholukova <62962282+gozel-cholukova@users.noreply.github.com>
2020-09-22 00:48:45 +0300

    Add files via upload

--- js js.js
+++ js js.js
@@ -7,3 +7,65 @@ const TypeWriter = function(txtElement, words, wait = 3000) {
   this.type();
   this.isDeleting = false;
 }
+
+//type method
+TypeWriter.prototype.type = function () {
+
+  // current index of word
+  const current = this.wordIndex % this.words.length;
+
+  // get current text of currrent word
+  const fullTxt = this.words[current];
+
+  // check if deleting
+  if(this.isDeleting) {
+  //remove char
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    //add char
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  //insert txt into element
+  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+  //initial type speed
+  let typeSpeed = 300;
+
+  if(this.isDeleting) {
+    typeSpeed /= 2;
+  } 
+
+  // if word is complete
+  if(!this.isDeleting && this.txt === fullTxt) {
+    //make pause at end
+    typeSpeed = this.wait;
+
+    //set delete to true
+    this.isDeleting = true;
+  } else if(this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+
+    //move to next word
+    this.wordIndex++;
+
+    //pause before start typing
+    typeSpeed = 500;
+  }
+
+
+  setTimeout(() => this.type(), typeSpeed)
+}
+
+//init on DOM load
+document.addEventListener('DOMContentLoaded', init);
+
+//init app
+function init() {
+  const txtElement = document.querySelector('.txt-type');
+  const words = JSON.parse(txtElement.getAttribute('data-words'));
+  const wait = txtElement.getAttribute('data-wait');
+
+  //ini typewriter
+  new TypeWriter(txtElement, words, wait);
+}
